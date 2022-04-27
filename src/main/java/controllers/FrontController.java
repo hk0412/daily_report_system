d9594d432a5ccc108866ff11ab9c6b12016a4856
hikari.kouno <hkr1118xxx@gmail.com>
2022-04-28 01:29:21 +0900

    Reportモデル、ReportViewモデル作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -13,6 +13,10 @@ import actions.ActionBase;
 import actions.UnknownAction;
 import constants.ForwardConst;
 
+/**
+ * フロントコントローラ
+ *
+ */
 @WebServlet("/")
 public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
